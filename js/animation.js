ac111cb6a771c60d7ba28b09050ce42db95928e8
Albert Arrieta <albert.arrieta@uleth.ca>
2023-11-29 20:46:12 -0700

    more rocking animation:

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,7 @@
 var tl = gsap.timeline({repeat: -1, yoyo: true});
 tl.to(".handle", {rotation: 8, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
 tl.to(".handle", {rotation: -3, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
-tl.to(".handle", {rotation: 6, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
+tl.to(".handle", {rotation: 2, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
+tl.to(".handle", {rotation: -2, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
+tl.to(".handle", {rotation: 3, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
 tl.to(".handle", {rotation: -8, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
