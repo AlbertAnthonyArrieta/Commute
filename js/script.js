ef75c4b3f13d090248a8705880be80fafec3361c
Albert Arrieta <albert.arrieta@uleth.ca>
2023-11-30 18:49:01 -0700

    full train

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Run this function every minute
+// setInterval(function() {
+//     var now = new Date();
+//     var hours = now.getHours();
+//     var minutes = now.getMinutes();
+
+//     // Check if it's midnight
+//     if (hours === 0 && minutes === 10) {
+//         // Make Josh disappear
+//         var josh = document.getElementById('josh');
+//         josh.style.display = "none";
+//     }
+// }, 1000);
