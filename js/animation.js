cef4117dae5e06eb922c4377c4639f9d977fe364
Albert Arrieta <albert.arrieta@uleth.ca>
2023-11-30 19:35:19 -0700

    isolated characters

--- js/animation.js
+++ js/animation.js
@@ -5,3 +5,6 @@ tl.to(".handle", {rotation: 2, transformOrigin: "top center", ease: "power1.inOu
 tl.to(".handle", {rotation: -2, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
 tl.to(".handle", {rotation: 3, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
 tl.to(".handle", {rotation: -8, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
+
+var t2 = gsap.timeline({repeat: -1, yoyo: true});
+t2.to("#erwin", {rotation: -3, transformOrigin: "top center", ease: "power1.inOut", duration: 0.5});
